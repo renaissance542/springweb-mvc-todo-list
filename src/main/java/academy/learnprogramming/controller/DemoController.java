a7e30d8b62054ad0bfaa227f6a534275dea2c80a
philipperoy <renaissance542@gmail.com>
2021-01-18 14:57:10 -0500

    Initial project set up with initializer, controller, and service

--- src/main/java/academy/learnprogramming/controller/DemoController.java
+++ src/main/java/academy/learnprogramming/controller/DemoController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Slf4j
@@ -38,10 +39,12 @@ public class DemoController {
         return "goodbye";
     }
     
-    // http://localhost:8080/todo-list/welcome
+    // http://localhost:8080/todo-list/welcome   <-- this is before the @RequestParam was used
+    // http://localhost:8080/todo-list/welcome?user=Phil&age=35  <-- this provides @RequestParam String user
     @GetMapping("welcome")  // viewResolver builds prefix(/WEB-INF/view/) + view(welcome) + suffix (.jsp)
-    public String welcome(Model model){
-        model.addAttribute("helloMessage", demoService.getHelloMessage("Phil"));
+    public String welcome(@RequestParam String user, @RequestParam int age, Model model){
+        model.addAttribute("helloMessage", demoService.getHelloMessage(user));
+        model.addAttribute("age", age);
         log.info("model = {}", model);
         return "welcome";
     }
--- src/main/webapp/WEB-INF/view/welcome.jsp
+++ src/main/webapp/WEB-INF/view/welcome.jsp
@@ -5,6 +5,7 @@
 <body>
     <h1>${helloMessage}</h1>
     <h2>${welcomeMessage}</h2>
+    <h3>You are ${age} years old</h3>
 </body>
 
 </html>
