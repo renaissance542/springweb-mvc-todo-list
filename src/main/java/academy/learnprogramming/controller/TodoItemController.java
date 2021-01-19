6b59a8bb2eaf890500b40ffa22eef37e598895f3
philipperoy <renaissance542@gmail.com>
2021-01-19 08:49:06 -0500

    TodoItemController added along with ViewNames and Mappings classes

--- src/main/java/academy/learnprogramming/controller/TodoItemController.java
+++ src/main/java/academy/learnprogramming/controller/TodoItemController.java
@@ -1,4 +1,42 @@
 package academy.learnprogramming.controller;
 
+import academy.learnprogramming.model.TodoData;
+import academy.learnprogramming.util.Mappings;
+import academy.learnprogramming.util.ViewNames;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+@Slf4j
+@Controller
 public class TodoItemController {
+    
+    // == fields ==
+        // TodoData service field
+    
+    
+    // == constructors ==
+        // instantiates  the TodoData service
+    
+    // == handler methods ==
+    
+    // http://localhost:8080/todo-list/items
+    @GetMapping(Mappings.ITEMS)
+    public String items (Model model){
+        
+        log.info("model = {}", model);
+        return ViewNames.ITEMS_LIST;
+    }
+    
+    // == model attributes ==
+    
+    @ModelAttribute("todoList")
+    public TodoData todoData (){
+        return new TodoData();
+    }
+        // map model to .jsp files and add attributes to the model
+        // call services to manipulate data to be added to the model
+        // the .jsp page will simply call the data that has already been modified
 }
